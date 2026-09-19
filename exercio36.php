2605a0c6f164b0e9f0e3ea597aa12fbba6fc7ad8
jeanc521 <jeanromoaldoc@gmail.com>
2026-09-18 21:34:56 -0300

    Add files via upload

---
+++ exercio36.php
@@ -0,0 +1,14 @@
+<?php
+    echo "Digite seu codigo de consumo: ";
+    $codigo_de_consumo  = (float) readLine();
+    echo "Digite o preço do kWh: ";
+    $kWh = (float) readLine();
+    echo "Digite a quantidade de consumida de kWh";
+    $quantidade_kWh = (float) readLine();
+
+
+    $calculo_energia = $kWh + $quantidade_kWh;
+    echo "A energia consumida e de: " + $calculo_kWh;
+
+
+?> 
