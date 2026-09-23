9e026fe02311eda07bf04e05a3789038957d11fe
joaosubtilrakoski <159147272+joaosubtilrakoski@users.noreply.github.com>
2026-09-23 16:14:16 -0300

    Add files via upload

---
+++ exercicio01.php
@@ -0,0 +1,29 @@
+<?php
+/*
+***************************
+Curso: Tecnologia em Análise e Desenvolvimento de Sistemas
+Disciplina: Linguagem e Técnicas de Programação
+Professor: Flores
+Turma: ADS2A
+
+Componentes:
+26013478-2 - Davi Ferreira da Costa
+26008617-2 - Fernando Cesar Cravo
+26008668-2 - Gabriel Eduardo Consorte
+26010149-2 - Jean Carlos
+26007982-2 - João Gabriel Subtil
+
+Data: 23 de Setembro de 2026
+
+Descritivo:
+01: Exiba a mensagem "Aprendendo PHP!!!"
+***************************
+*/
+
+echo "Aprendendo PHP!!!";
+
+/*
+Explicação:
+O programa exibe a mensagem "Aprendendo PHP!!!" na tela.
+*/
+?>
